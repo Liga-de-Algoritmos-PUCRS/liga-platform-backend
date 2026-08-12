0f11689ab974ed940d0c5c140f85a70549ff133e
Guilherme Cassol <guicassol9@hotmail.com>
2026-08-11 23:45:33 -0300

    chore: remove dependencia nao usada e arquivo duplicado
    `GetUserByIdService` injetava o `CryptographyAdapter` sem nunca usar, e
    `get-user.decorator copy.ts` era copia literal do original, sem nenhum
    import apontando para ela.

--- src/modules/User/application/services/get-user.service.ts
+++ src/modules/User/application/services/get-user.service.ts
@@ -2,14 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { UserRepository } from '../../domain/user.repository';
 import { User } from '@/modules/User/domain/user.entity';
 import { ExceptionsAdapter } from '@/infrastructure/Exceptions/exceptions.adapter';
-import { CryptographyAdapter } from '@/infrastructure/Criptography/cryptography.adapter';
 import { UserExceptions } from '@/infrastructure/Exceptions/exceptions.types';
 
 @Injectable()
 export class GetUserByIdService {
   constructor(
     private readonly userRepository: UserRepository,
-    private readonly cryptographyAdapter: CryptographyAdapter,
     private readonly exceptionsAdapter: ExceptionsAdapter,
   ) {}
 
